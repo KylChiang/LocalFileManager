4fea5391454544e5e1c6e0cc2240e49c59b29d06
KylChiang <chiangkaiyun@gmail.com>
2020-09-07 20:17:24 +0800

    Add init files

---
+++ LocalFileManager/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  LocalFileManager
+//
+//  Created by Kyl Chiang on 2020/9/7.
+//  Copyright © 2020 Play4Act Studio. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
